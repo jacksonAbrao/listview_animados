27d060c879970770251011bce63297a2d255ced9
jacksonAbrao <jackson.comp1681@hotmail.com>
2022-05-23 23:25:30 -0300

    finalizando o "projeto"

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,83 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  late List<String> categoria;
+  late List<String> items;
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  HomePage({
+    Key? key,
+  }) : super(key: key) {
+    categoria = List.generate(10, (i) => "Categora $i");
+    items = List.generate(5, (i) => "Item $i");
+  }
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 200,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text(
+                  "App Bar ListView Animado",
+                ),
+                expandedTitleScale: 1.4,
+                titlePadding:
+                    const EdgeInsets.only(top: 16, bottom: 16, left: 8),
+                background: Image.asset(
+                  'assets/tristana.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: categoria.length,
+                  itemBuilder: (_, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            categoria[index],
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: items.length,
+                            itemBuilder: (_, index) {
+                              return ListTile(
+                                title: Text(
+                                  items[index],
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              );
+                            },
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ]),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
